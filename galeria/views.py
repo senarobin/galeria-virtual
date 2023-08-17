b64848179d71d25a99d58ab757b4e8c193bac43f
senarobin <sena72454@gmail.com>
2023-08-17 15:38:20 -0300

    atualizacao

--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse
+    return HttpResponse('<h1>Projeto de site do L3 system</h1>')
     
     
     
